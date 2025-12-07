420b6e4fee218f44fd80bf06229d3bb22ee4b40b
atk66 <alexthorir@gmail.com>
2025-12-07 15:17:09 +0000

    2

---
+++ quiz-app/src/Components/Quiz/Quiz.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import './Quiz.css'
+
+const Quiz = () => {
+  return (
+    <div className='container'>
+        <h1>Quiz App</h1>
+        <hr />
+        <h2>Spurning</h2>
+        <ul>
+            <li>svar</li>
+            <li>svar</li>
+            <li>svar</li>
+            <li>svar</li>
+        </ul>
+
+    </div>
+  )
+}
+
+export default Quiz
